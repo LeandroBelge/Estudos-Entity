0509c29ed63e35e5e8f83ce38c6142455b3068b4
Leandro Belge da Cunha <leandrobelge@gmail.com>
2016-08-22 13:50:20 -0300

    Iniciando os estudos

---
+++ EstudandoEntity/EstudandoEntity/Infra/EntidadesContext.cs
@@ -0,0 +1,20 @@
+﻿using EstudandoEntity.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace EstudandoEntity.Infra
+{
+    public class EntidadesContext : DbContext
+    {        
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        public EntidadesContext(string conexao) : base(conexao)
+        {
+            Database.SetInitializer<EntidadesContext>(null);
+        }
+    }
+}
---
+++ EstudandoEntity/EstudandoEntity/Program.cs
@@ -0,0 +1,28 @@
+﻿using EstudandoEntity.Entidades;
+using EstudandoEntity.Infra;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstudandoEntity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string conexao = ConfigurationManager.ConnectionStrings["LojaConnectionString"].ConnectionString;
+            EntidadesContext contexto = new EntidadesContext(conexao);
+            Usuario renan = new Usuario()
+            {
+                Nome = "Renan",
+                Senha = "123"
+            };
+            contexto.Usuarios.Add(renan);
+            contexto.SaveChanges();
+            contexto.Dispose();
+        }
+    }
+}
